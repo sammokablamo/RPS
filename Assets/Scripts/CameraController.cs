a2e3f4b977450e57dfbc5b21905cbfc1aa37a897
Samuel Wey <sam.wey@gmail.com>
2013-12-23 11:33:45 -0800

    creating new rock paper scissors project, already has Ouya implementation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player;
+	private Vector3 offset;
+	// Update is called once per frame
+
+	void Start ()
+	{
+		offset = transform.position;
+	}
+
+	//LateUpdate is called after all Update functions have been called. This is useful to order script execution. For example a follow camera should always be implemented in LateUpdate because it tracks objects that might have moved inside Update.
+
+	void LateUpdate () 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	//gui text variable
+	public GUIText countText;
+	public GUIText winText;
+
+	//counter
+	private int count;
+	//start is called on the frame when a scrip tis enabled just before any of the update methods are called for the first time.
+	void Start ()
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+	//fixed update is called every fixed framerate frame if monobehavior is enabled.
+	void FixedUpdate ()
+	{
+		//player controls
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+
+		//pickup stuff
+		//when player controller collides with an object, create a collider variable and name it "other".
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		//if the collider variable named other has a tag that's equal to "pickup" then...
+		if(other.gameObject.tag == "PickUp")
+		{
+			//set active to false, turning it off.
+			other.gameObject.SetActive(false);
+			//increment count by one.
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 16) 
+		{
+			winText.text = "YOU WIN!";
+		}
+	}
+
+}
