d458103f0d75e8d6016657087d70e849afeb4042
Samuel Wey <sam.wey@gmail.com>
2013-12-23 13:31:44 -0800

    got it running on Ouya, setup dependencies, built settings, player settings, wifi settings.

--- Assets/Plugins/Android/src/OuyaUnityApplication.java
+++ Assets/Plugins/Android/src/OuyaUnityApplication.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.Company.ProductName;
+package com.HotelRoosevelt.RockPaperScissor;
 
 import tv.ouya.console.api.OuyaController;
 import tv.ouya.sdk.*;
