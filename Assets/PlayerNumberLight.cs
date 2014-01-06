d995d5a684683bfa397ba58cffbac7157b23a27c
Samuel Wey <sam.wey@gmail.com>
2014-01-06 12:53:10 -0800

    player number lights follow player properly

--- Assets/PlayerNumberLight.cs
+++ Assets/PlayerNumberLight.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class PlayerNumberLight : MonoBehaviour {
-
+	public GameObject playerGameObject;
+	private Vector3 newPosition;
 	// Use this for initialization
 	void Start () {
-	
+	//setup reference to player
+
 	}
 	
 	// Update is called once per frame
@@ -15,7 +17,16 @@ public class PlayerNumberLight : MonoBehaviour {
 
 	void LateUpdate()
 	{
-		//transform.position = transform.position - Vector3(0, 1,0);
+		newPosition = new Vector3(playerGameObject.GetComponent<Transform>().position.x, playerGameObject.GetComponent<Transform>().position.y + 1.0f, playerGameObject.GetComponent<Transform>().position.z);
+		transform.position = newPosition;
+		if (playerGameObject.activeSelf == false)
+		{
+			light.intensity = 0;
+		}
+		if (playerGameObject.activeSelf == true)
+		{
+			light.intensity = 4;
+		}
 		//transform.LookAt( Vector3.up, Vector3.up);
 	}
 }
